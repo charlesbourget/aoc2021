c3fcdc10b48dfe826b329585574d892ea3d335f2
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2021-12-02 13:19:08 -0500

    Damn you js with your dynamic types

--- add-stars.mjs
+++ add-stars.mjs
@@ -7,6 +7,6 @@ let current_value = (
   await $`grep -E -o -e 'message=\\d+' README.md`
 ).toString();
 
-const new_value = parseInt(current_value.split("=")[1]) + number;
+const new_value = parseInt(current_value.split("=")[1]) + parseInt(number);
 
 await $`perl -i -pe"s/message=\\d+/message=${new_value}/g" README.md`;
