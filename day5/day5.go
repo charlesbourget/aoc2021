f101307c304e2c8fda7128157e1586ca76f560a4
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2021-12-05 14:42:51 -0500

    Day 5

---
+++ day5/day5.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"github.com/charlesbourget/aoc2021/lib"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := lib.Read("day5/input")
+	if err != nil {
+		fmt.Println("Error while reading input. ", err)
+		return
+	}
+
+	fmt.Printf("Part 1: %d\n", Part1(input))
+	fmt.Printf("Part 2: %d\n", Part2(input))
+}
+
+type line struct {
+	x1 int
+	y1 int
+	x2 int
+	y2 int
+}
+
+func Part1(input []string) int {
+	lines := parse(input, false)
+	count := mapLines(lines, false)
+
+	return count
+}
+
+func Part2(input []string) int {
+	lines := parse(input, true)
+	count := mapLines(lines, true)
+
+	return count
+}
+
+func parse(input []string, diag bool) (lines []line) {
+	for _, v := range input {
+		coordo := strings.Split(v, " -> ")
+		initial := strings.Split(coordo[0], ",")
+		final := strings.Split(coordo[1], ",")
+		x1, _ := strconv.Atoi(initial[0])
+		y1, _ := strconv.Atoi(initial[1])
+		x2, _ := strconv.Atoi(final[0])
+		y2, _ := strconv.Atoi(final[1])
+
+		lines = append(lines, line{x1, y1, x2, y2})
+	}
+
+	return
+}
+
+func mapLines(lines []line, diag bool) (count int) {
+	array := make([][]uint8, 1000)
+	for i := range array {
+		array[i] = make([]uint8, 1000)
+	}
+
+	for _, l := range lines {
+		if l.x1 == l.x2 {
+			if l.y1 < l.y2 {
+				for i := l.y1; i <= l.y2; i++ {
+					array[l.x1][i] += 1
+					if array[l.x1][i] == 2 {
+						count++
+					}
+				}
+			} else {
+				for i := l.y1; i >= l.y2; i-- {
+					array[l.x1][i] += 1
+					if array[l.x1][i] == 2 {
+						count++
+					}
+				}
+			}
+		} else if l.y1 == l.y2 {
+			if l.x1 < l.x2 {
+				for i := l.x1; i <= l.x2; i++ {
+					array[i][l.y1] += 1
+					if array[i][l.y1] == 2 {
+						count++
+					}
+				}
+			} else {
+				for i := l.x1; i >= l.x2; i-- {
+					array[i][l.y1] += 1
+					if array[i][l.y1] == 2 {
+						count++
+					}
+				}
+			}
+		} else if diag {
+			if l.x1 < l.x2 {
+				if l.y1 < l.y2 {
+					for i := 0; i <= l.x2-l.x1; i++ {
+						array[l.x1+i][l.y1+i] += 1
+						if array[l.x1+i][l.y1+i] == 2 {
+							count++
+						}
+					}
+				} else {
+					for i := 0; i <= l.x2-l.x1; i++ {
+						array[l.x1+i][l.y1-i] += 1
+						if array[l.x1+i][l.y1-i] == 2 {
+							count++
+						}
+					}
+				}
+			} else {
+				if l.y1 < l.y2 {
+					for i := 0; i <= l.x1-l.x2; i++ {
+						array[l.x1-i][l.y1+i] += 1
+						if array[l.x1-i][l.y1+i] == 2 {
+							count++
+						}
+					}
+				} else {
+					for i := 0; i <= l.x1-l.x2; i++ {
+						array[l.x1-i][l.y1-i] += 1
+						if array[l.x1-i][l.y1-i] == 2 {
+							count++
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return count
+}
