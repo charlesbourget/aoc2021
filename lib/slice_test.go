f6f9810fc7be2b16e66a7ec900ec9b6a1653d9b0
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2021-12-07 08:45:15 -0500

    Tests

--- lib/slice_test.go
+++ lib/slice_test.go
@@ -26,7 +26,7 @@ func TestUnpackInt(t *testing.T) {
 	expected2 := 42
 	result1, result2 := UnpackInt([]string{"100", "42"})
 	if result1 != expected1 || result2 != expected2 {
-		t.Fatalf(`Unpack() = %d, %d, want %d, %d, error`, result1, result2, expected2, expected1)
+		t.Fatalf(`UnpackInt() = %d, %d, want %d, %d, error`, result1, result2, expected2, expected1)
 	}
 }
 
@@ -34,6 +34,15 @@ func TestToIntSliceTest(t *testing.T) {
 	expected := []int{0, 1, 2, 3}
 	result := ToIntSlice([]string{"0", "1", "2", "3"})
 	if !reflect.DeepEqual(result, expected) {
-		t.Fatalf(`Unpack() = %d, want %d, error`, result, expected)
+		t.Fatalf(`ToIntSlice() = %d, want %d, error`, result, expected)
+	}
+}
+
+func TestMinMax(t *testing.T) {
+	expectedMin := 0
+	expectedMax := 17
+	resultMin, resultMax := MinMax([]int{0, 1, 17, 3})
+	if resultMin != expectedMin || resultMax != expectedMax {
+		t.Fatalf(`MinMax() = %d, %d, want %d, %d, error`, resultMin, resultMax, expectedMin, expectedMax)
 	}
 }
