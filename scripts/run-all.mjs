1dc50a4f38ac9be652073df379cac36c57be1571
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2021-12-08 10:41:26 -0500

    having way to much fun with JS

--- scripts/run-all.mjs
+++ scripts/run-all.mjs
@@ -9,6 +9,7 @@ cd("..");
 await $`mkdir -p bin`;
 await $`mkdir -p out`;
 const data = {};
+let total = 0;
 
 const paths = await globby("../day*", {
   expandDirectories: true,
@@ -24,9 +25,12 @@ for (const path of paths) {
   data[name] = dayData.results[0];
   delete data[name]["times"];
   delete data[name]["exit_codes"];
-
-  console.log(chalk.green(`${name}: ${data[name]["mean"]} s`));
+  const mean = (data[name]["mean"] * 1000)
+  total += mean
+  console.log(chalk.yellow(`${name}: ${mean.toFixed(2)} ms`));
 }
+
+console.log(chalk.green(`Total: ${total.toFixed(2)} ms`));
 
 fs.writeFileSync("../out/results.json", JSON.stringify(data));
 
