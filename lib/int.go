f47ca5d3ef45de415c7a6b442f4cf87672085649
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2021-12-15 19:54:44 -0500

    Day 15

--- lib/int.go
+++ lib/int.go
@@ -1,8 +1,15 @@
 package lib
 
+import "math"
+
 func Abs(x int) int {
 	if x < 0 {
 		return -x
 	}
 	return x
+}
+
+func MinInt(x int, y int) int {
+	min := math.Min(float64(x), float64(y))
+	return int(min)
 }
